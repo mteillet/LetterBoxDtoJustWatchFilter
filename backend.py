0fb66c28dd4f6e4e4268c99d3caade8ea26d19de
mteillet <martin.teillet@hotmail.fr>
2023-12-25 19:41:52 +0100

    new file:   requirements.txt

---
+++ backend.py
@@ -0,0 +1,80 @@
+# backend.py
+from bs4 import BeautifulSoup
+import requests
+import re
+
+def main():
+    '''
+    Main Function that will be replaced with frontend / gui call
+    '''
+    letterBoxdListLink = 'https://letterboxd.com/mteillet/list/solo-watch-list/'
+    # Getting the list
+    filmList = getList(letterBoxdListLink)
+    # Print only if you need to debug what's been found in the letterboxd parsing
+    logsLetterBoxData(filmList)
+    # Comparing with justwatch database
+    justwatchCompare(filmList)
+
+def getList(listLink):
+    '''
+    Get the contents from a letterBoxd list and returns it
+    '''
+    listPage = requests.get(listLink)
+
+    # Check there was no error getting the list 
+    if listPage.status_code != 200:
+        return print("ERROR LOADING THE LINK")
+
+    soup = BeautifulSoup(listPage.content ,features="html.parser")
+    filmSoup = (soup.find_all("li", class_="poster-container"))
+    filmList = []
+    for film in filmSoup:
+        #print(film)
+        posterContainer = film.find("div", class_="really-lazy-load")
+        #print(posterContainer.find("div", class_="data-film-slug"))
+        regex = re.compile('data-film-slug=["\'](.*?)["\']')
+        movieName = (regex.search(str(posterContainer)).group(1))
+        filmList.append(movieName)
+
+    return filmList
+
+def logsLetterBoxData(filmList):
+    '''
+    Inspects the contents found on the letterboxd film lists, returns the move names and the number of films
+    '''
+    print("Here is the films founds :")
+    for film in filmList:
+        print(film)
+    
+    print("\nFound %i films in the LetterBoxD list" % (len(filmList)))
+
+def justwatchCompare(filmList):
+    '''
+    Checks the availibility of the movies with justwatch database
+    '''
+    justWatchURL = 'https://www.justwatch.com/fr'
+    
+    # Temp test on single movie
+    filmList = ["sicario-2015"]
+
+    for movie in filmList:
+        # Get the letterboxd film page
+        letterBoxLink = "https://letterboxd.com/film/%s/" % movie
+        filmLetterBoxPage = requests.get(letterBoxLink)
+
+        # Check there was no error getting the list 
+        if filmLetterBoxPage.status_code != 200:
+            return print("ERROR LOADING THE LINK")
+        
+        soup = BeautifulSoup(filmLetterBoxPage.content, features="html.parser")
+        print(soup)
+        '''
+        filmSoup = soup.find("a", class_="jw-branding")
+        print(filmSoup)
+        filmSoup = soup.find("section", class_="watch-panel js-watch-panel")
+        print(filmSoup)
+        '''
+
+
+if __name__ == "__main__":
+    main()
