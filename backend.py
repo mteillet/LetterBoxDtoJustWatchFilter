3dfb584d679b1d90e60ec54d63db5a2355dc2088
mteillet <martin.teillet@hotmail.fr>
2024-01-02 08:43:24 +0100

    Added a duplicate of justwatchcompare for GUI versison in order to get a progressbar 	modified:   backend.py

--- backend.py
+++ backend.py
@@ -155,6 +155,94 @@ def justwatchCompare(filmList):
 
     return(filmDict, servicesList)
 
+def justwatchCompareGui(filmList, progress_callback):
+    '''
+    Checks the availibility of the movies with justwatch database
+    '''
+    justWatchURL = 'https://www.justwatch.com/fr'
+    
+    # Temp test on single movie
+    #filmList = ["boyhood", "the-skin-i-live-in", "kairo", "fargo", "yi-yi", "possession", "kanikosen"]
+
+    filmDict = {}
+    servicesList = []
+
+    current = 1
+    for movie in filmList:
+        progress_value = int((current/len(filmList))*100)
+        progress_callback.emit(progress_value, movie)
+        print("Scanning %i / %i, ---> %s" % (current, len(filmList), movie))
+        filmDict[movie] = {}
+        # Get the letterboxd film page
+        justWatchLink = "%s/recherche?q=%s" % (justWatchURL, movie.replace("-", " "))
+        justWatchSearch = requests.get(justWatchLink)
+
+        # Check there was no error getting the list 
+        if justWatchSearch.status_code != 200:
+            filmDict[movie]["Error"] = True
+        else:
+            filmDict[movie]["Error"] = False
+        
+            soup = BeautifulSoup(justWatchSearch.content, features="html.parser")
+            firstRow = soup.find("div", class_="title-list-row__row")
+            #print(firstRow)
+            if firstRow is not None:
+                filmSoup = firstRow.find("a", class_="title-list-row__column-header")
+            else:
+                #print("None found for %s" % movie)
+                filmSoup = None
+            # print(filmSoup)
+            #filmSoup = firstRow.find("article", class_="buybox")
+            # Try except in case nothing exists no film is listed for this search
+            try :
+                spanTitle = filmSoup.find("span", class_="header-title")
+                filmDict[movie]["jwTitle"] = spanTitle.text
+            except AttributeError:
+                filmDict[movie]["jwTitle"] = "NOT FOUND"
+            
+            # STREAMING SERVICES
+            # Checking only first row in order to make sure if a service (stream or rent)
+            # is missing, it does not return the results from the second row
+            # Try except in case nothing exists no film is listed for this search
+            try:
+                filmSoup = firstRow.find("article", class_="buybox")
+                streamSoup = filmSoup.find("div", class_="buybox-row stream inline")
+            except AttributeError:
+                streamSoup = None
+            # In case ne streaming service is available
+            if streamSoup is None:
+                imgs = ['alt="None"']
+            else:
+                imgs = streamSoup.find_all("img")
+            #print(streamSoup)
+            filmDict[movie]["streaming"] = []
+            for img in imgs:
+                #print(img)
+                regex = re.compile('alt=["\'](.*?)["\']')
+                streamingService = regex.search(str(img)).group(1)
+                filmDict[movie]["streaming"].append(streamingService)
+                if streamingService not in servicesList:
+                    servicesList.append(streamingService)
+
+            # RENTING SERVICES
+            rentSoup = soup.find("div", class_="buybox-row rent inline")
+            if rentSoup is None:
+                imgs = ['alt="None"']
+            else:
+                imgs = rentSoup.find_all("img")
+            filmDict[movie]["rent"] = []
+            for img in imgs:
+                #print(img)
+                regex = re.compile('alt=["\'](.*?)["\']')
+                rentingService = regex.search(str(img)).group(1)
+                filmDict[movie]["rent"].append(rentingService)
+                if rentingService not in servicesList:
+                    servicesList.append(rentingService)
+
+        current += 1
+
+    return(filmDict, servicesList)
+
 def printInfosOnJustWatchList(filmDict, servicesList):
     for film in filmDict:
         if filmDict[film]["jwTitle"] == "NOT FOUND":
